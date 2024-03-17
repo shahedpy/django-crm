f669a928ceb794b5acda178d49be0f6f4a1bac3b
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2024-03-17 20:40:20 +0600

    model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Record(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = PhoneNumberField()
+    address = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
