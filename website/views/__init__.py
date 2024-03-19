8f718475c17a0907d1602ad1aafeb2a281228e25
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2024-03-19 11:32:43 +0600

    views directory

---
+++ website/views/__init__.py
@@ -0,0 +1,19 @@
+from .all import (
+    home,
+    logout_user,
+    register_user,
+    customer_record,
+    delete_record,
+    add_record,
+    update_record,
+)
+
+all = (
+    home,
+    logout_user,
+    register_user,
+    customer_record,
+    delete_record,
+    add_record,
+    update_record,
+)
--- website/views/all.py
+++ website/views/all.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm, AddRecordForm
-from .models import Record
+from website.forms import SignUpForm, AddRecordForm
+from website.models import Record
 
 
 def home(request):
